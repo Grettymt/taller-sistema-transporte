8c79deb59cc62a605349628f779d5490dd7bbae0
Gretty Mosquera <gretymosquera@gmail.com>
2023-02-02 10:47:17 -0500

    Se inicializa el taller final - sistema de transporte

---
+++ src/main/java/com/co/sofka/appsistematransporte/AppSistemaTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.co.sofka.appsistematransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSistemaTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSistemaTransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/sofka/appsistematransporte/AppSistemaTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.sofka.appsistematransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSistemaTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
